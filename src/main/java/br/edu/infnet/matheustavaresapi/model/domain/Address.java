6d36c4e6ef20a221623921b8474785bdeae95753
Matheus Tavares <matheusce55@gmail.com>
2025-09-01 18:15:48 -0300

    feature: remove address from project

--- src/main/java/br/edu/infnet/matheustavaresapi/model/domain/Address.java
+++
@@ -1,8 +0,0 @@
-package br.edu.infnet.matheustavaresapi.model.domain;
-
-public class Address {
-    public String street;
-    public String City;
-    public String Country;
-    public String ZipCode;
-}
